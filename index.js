b4572f9e0bd39493662cae47c5e33b8b074de9d9
Precious1324 <aleriwaprecious70@gmail.com>
2024-02-13 21:00:30 +0100

    Waitlist Added

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express()
 
 
 app.use(cors({
-    origin: ["http://localhost:5173", ""]
+    origin: ["http://localhost:5173", "https://lecturepodacast.netlify.app"]
 }))
 app.use(express.json())
 
